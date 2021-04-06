72ab59a610fa8688b0119c2c401def147aed1499
weissenh <50957092+weissenh@users.noreply.github.com>
2021-04-06 14:49:23 +0200

    Started implementing reader for opennmt file format
    needs testing

--- evaluator/readers.py
+++ evaluator/readers.py
@@ -3,18 +3,28 @@
 """
 Different readers for different file formats, readers output CorpusInstance s
 
-author: weissenh ( piaw@coli.uni-saarland.de )
+author: weissenh
 test environment: Ubuntu 20.04, Python 3.7
 date: April 2020
 """
 
 # todo: more readers (also for opennmt file format?), maybe turn into classes?
+# todo: test/implement opennmt reader
+# todo: demo should test more readers
 # todo: documentation missing
 
 import csv  # reading TSV files
 import sys
 import os
 from corpus_instance import CorpusInstance
+
+
+def check_input_files_existance(input_files: list) -> None:
+    # raises Error if one of the files is not found (input_files: List[str] )
+    for file_path in input_files:
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError(f"Input file not found: {file_path}")
+    return
 
 
 def get_samples(file_path: str):  # -> Iterator
@@ -25,8 +35,7 @@ def get_samples(file_path: str):  # -> Iterator
     :return: Iterator of CorpusInstance
     """
     # todo should we have the line number here or not?
-    if not os.path.isfile(file_path):
-        raise FileNotFoundError(f"Input file not found: {file_path}")
+    check_input_files_existance([file_path])
     with open(file_path, encoding='utf-8', mode='r') as infile:
         for line_num, row in enumerate(csv.reader(infile, delimiter="\t")):
             sentence, representation, gen_type_required = row
@@ -40,9 +49,7 @@ def get_samples_from_two_files(goldfile: str, systemfile):  # -> Iterator
     # todo should we raise runtime error if files of unequal length???
     # currently just silent
     # todo should we have the line number here or not?
-    for file_path in [goldfile, systemfile]:
-        if not os.path.isfile(file_path):
-            raise FileNotFoundError(f"Input file not found: {file_path}")
+    check_input_files_existance([goldfile, systemfile])
     with open(goldfile, encoding='utf-8', mode='r') as goldf, \
             open(systemfile, encoding='utf-8', mode='r') as systemf:
         goldreader = csv.reader(goldf, delimiter="\t")
@@ -66,6 +73,25 @@ def get_samples_from_two_files(goldfile: str, systemfile):  # -> Iterator
                                           line_num=line_num)
             # todo should I test here for equality of first row?
             yield goldsample, systemsample
+
+
+# todo: test this function (opennmtl files)
+def get_samples_from_opennmt_file(opennmtfile: str):
+    # assumes: source target model-prediction   tab separated
+    # yields goldsample, systemsample pair
+    # note: gen_type_required info not available!
+    check_input_files_existance([opennmtfile])
+    with open(opennmtfile, encoding='utf-8', mode='r') as infile:
+        for line_num, row in enumerate(csv.reader(infile, delimiter="\t")):
+            source, target, model_pred = row
+            yield CorpusInstance(sentence=source,
+                                 logical_form=target,
+                                 gen_type_required="",
+                                 line_num=line_num), \
+                  CorpusInstance(sentence=source,
+                                 logical_form=model_pred,
+                                 gen_type_required="",
+                                 line_num=line_num)
 
 
 def main(argv):
